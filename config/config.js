b5e0cc25bb7a94d9285e7747e859cd95ce7d0920
Alaric Hadef <alaric.hadef@weezevent.com>
2021-08-07 20:40:23 +0200

    test ci

--- config/config.js
+++ config/config.js
@@ -1,37 +1,37 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      bigNumberStrings: true,
+    development: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            bigNumberStrings: true
+        }
+    },
+    test: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            bigNumberStrings: true
+        }
+    },
+    production: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            bigNumberStrings: true
+        }
     }
-  },
-  test: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      bigNumberStrings: true,
-    }
-  },
-  production: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      bigNumberStrings: true,
-    }
-  }
 };
